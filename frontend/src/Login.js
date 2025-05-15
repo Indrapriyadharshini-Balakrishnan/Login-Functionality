07fb6be641bd0359b928d7278e890490e95b32c1
Indrapriyadharshini-Balakrishnan <indrapriya19@gmail.com>
2025-05-15 14:21:04 +0200

    Updated with env url /app/login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${API}`, {
+      const res = await axios.post(`${API}/api/login`, {
         username,
         password,
       });
